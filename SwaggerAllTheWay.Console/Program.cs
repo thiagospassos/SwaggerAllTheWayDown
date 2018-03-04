8f5e934baa3f4c8f2cb18f6f5b929ca6c14c60e7
Thiago Passos <thiago@passos.com.au>
2018-03-04 22:52:46 +1000

    adding console

---
+++ SwaggerAllTheWay.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SwaggerAllTheWay.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Client.SampleDataClient { BaseUrl = "http://localhost:5000" };
+            var forecasts = client.WeatherForecastsAsync().GetAwaiter().GetResult();
+
+            foreach (var forecast in forecasts)
+            {
+                System.Console.WriteLine($"{forecast.Summary} : {forecast.TemperatureC}c");
+            }
+        }
+    }
+}
